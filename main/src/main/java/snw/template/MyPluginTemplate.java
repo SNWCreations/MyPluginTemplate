1179de50f622d5acb1c96395a822645fdad3e1f1
ZX夏夜之风 <snwcreations@qq.com>
2024-11-23 18:24:29 +0800

    build: build script improvements
    Add ProGuard obfuscation support
    Add Dev-Build marker

--- main/src/main/java/snw/template/MyPluginTemplate.java
+++ main/src/main/java/snw/template/MyPluginTemplate.java
@@ -3,6 +3,10 @@ package snw.template;
 import org.bukkit.plugin.java.JavaPlugin;
 import snw.template.nms.AdapterRetriever;
 
+import java.io.IOException;
+import java.util.jar.JarFile;
+import java.util.logging.Level;
+
 // Don't use final modifier there if you use MockBukkit
 public class MyPluginTemplate extends JavaPlugin {
 
@@ -19,5 +23,16 @@ public class MyPluginTemplate extends JavaPlugin {
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    public boolean isDebugBuild() {
+        try {
+            try (final JarFile jarFile = new JarFile(getFile())) {
+                return jarFile.getManifest().getAttributes("Dev-Build") != null;
+            }
+        } catch (IOException e) {
+            getLogger().log(Level.SEVERE, "Error reading debug build attribute", e);
+            return false;
+        }
     }
 }
